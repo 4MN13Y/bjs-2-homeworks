0dc14ffe12ed0110fc457712c8c905b7ea7a8683
Yura <psnfifka@gmail.com>
2023-02-10 19:12:05 +0200

    commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,38 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = Math.pow(b, 2) - 4 * a * c;
+  if (d < 0) {
+    arr = [];
+  } else if (d == 0) {
+    arr.push(-b / (2 * a));
+  } else if (d > 0) {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let percentPerMonth = percent / 100 / 12;
+  let creditBody = amount - contribution;
+  let payPerMonth =
+    creditBody *
+    (percentPerMonth +
+      percentPerMonth / ((1 + percentPerMonth) ** countMonths - 1));
+  let totalTest = payPerMonth * countMonths;
+  let result = Number(totalTest.toFixed(2));
+  if (isNaN(percent)) {
+    return (result = `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`);
+  }
+
+  if (isNaN(Number(contribution))) {
+    return (result = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`);
+  }
+  // if (isNaN(Number(amount))) {
+  //   return (result = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`);
+  // }
+
+  return result;
 }
